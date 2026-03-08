9773bad57818719b25c75e35a021873103d5d20c
Bhuvanesh H <tamilshorts56@gmail.com>
2026-03-08 17:01:36 +0530

    Add Publisher code-signing signature and Icon to Setup to reduce false-positives

---
+++ AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SIMATS Browser Setup")]
+[assembly: AssemblyDescription("Official Download Manager for SIMATS Browser")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SIMATS University")]
+[assembly: AssemblyProduct("SIMATS Browser Fast Setup")]
+[assembly: AssemblyCopyright("Copyright © SIMATS 2026")]
+[assembly: AssemblyTrademark("SIMATS")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1a2b3c4d-5e6f-7a8b-9c0d-1e2f3a4b5c6d")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
